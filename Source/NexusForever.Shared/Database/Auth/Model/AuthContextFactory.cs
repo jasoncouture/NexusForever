2191362e9c58f310b4804d9d0203b5fa5c879da0
Jason Couture <jcouture@pssproducts.com>
2018-12-06 21:51:28 -0500

    Database package upgrade, started work on migraitons.

---
+++ Source/NexusForever.Shared/Database/Auth/Model/AuthContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using NexusForever.Shared.Configuration;
+
+namespace NexusForever.Shared.Database.Auth.Model
+{
+    public class AuthContextFactory : IDbContextFactory<AuthContext>
+    {
+        public AuthContext Create(DbContextFactoryOptions options)
+        {
+            return null;
+        }
+    }
+}
