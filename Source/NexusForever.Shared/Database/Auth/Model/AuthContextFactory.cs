b0717e5b4463af3b3eb0c41225d363aa2311e56e
Jason Couture <jcouture@pssproducts.com>
2018-12-06 23:55:24 -0500

    Preparation work for Migrations. Updated EF version, and MySql driver.

--- Source/NexusForever.Shared/Database/Auth/Model/AuthContextFactory.cs
+++ Source/NexusForever.Shared/Database/Auth/Model/AuthContextFactory.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using NexusForever.Shared.Configuration;
 
 namespace NexusForever.Shared.Database.Auth.Model
 {
-    public class AuthContextFactory : IDbContextFactory<AuthContext>
+    public class AuthContextFactory : IDesignTimeDbContextFactory<AuthContext>
     {
-        public AuthContext Create(DbContextFactoryOptions options)
+        public AuthContext CreateDbContext(string[] args)
         {
             return null;
         }
